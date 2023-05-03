7fe49f5ef001c743f751ffba94a353359753c71a
Juan David Ramirez Monje <thismonje@MacBook-Air-de-Juan.local>
2023-05-03 10:17:12 -0500

    fix bugs

--- src/components/commons/logo-header/styles/styles.tsx
+++ src/components/commons/logo-header/styles/styles.tsx
@@ -3,12 +3,12 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   container: {
     alignItems: 'center',
-    marginBottom: 20,
-    marginTop: 20,
+    marginBottom: 30,
+    marginTop: 70,
   },
   logo: {
     display: 'flex',
     justifyContent: 'center',
-    marginLeft: 22
+    marginLeft: 22,
   }
 });
