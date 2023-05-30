72b43a82812a45464509ab2a623f5d9ed98bd8bf
unknown <rjuandavid1002@gmail.com>
2023-05-30 16:48:11 -0500

    change library of render htmlc

--- src/components/commons/webview/components/webview-layout.tsx
+++ src/components/commons/webview/components/webview-layout.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import {StyleProp, ViewStyle, useWindowDimensions} from 'react-native';
-import RenderHTML from 'react-native-render-html';
+import {ActivityIndicator, ScrollView, StyleProp, ViewStyle} from 'react-native';
+import {WebView, WebViewMessageEvent} from 'react-native-webview';
+import {styles} from './../index';
+import {fontFace, normalize, HtmlCleaner} from './../utils';
 
 interface InlineWebViewProps {
   html: string;
@@ -9,38 +11,99 @@ interface InlineWebViewProps {
 }
 
 const InlineWebView: React.FC<InlineWebViewProps> = props => {
+  const [webViewHeight, setWebViewHeight] = React.useState<number>(100);
 
-  const { width } = useWindowDimensions();
+  const onWebViewMessage = (event: WebViewMessageEvent) => {
+    let height = Number(event.nativeEvent.data);
 
-  const source = {
-    html : props?.html
-  }
-
-  const renderersProps = {
-    img: {
-      enableExperimentalPercentWidth: true
+    if (height > 50) {
+      height = height + 20;
     }
+
+    setWebViewHeight(height);
   };
 
-  
   return (
-    <RenderHTML
-      contentWidth={width}
-      source={source}
-      tagsStyles={
-        {
-          p : {fontFamily : 'Nunito', fontSize : 18, color : '#061946', fontWeight : "800"}, 
-          span : {fontFamily : 'Nunito', fontSize : 18, color : '#061946', fontWeight : "800"}, 
-          li : {fontFamily : 'Nunito', fontSize : 14, color : '#061946', fontWeight : "800"}, 
-          ol : {fontFamily : 'Nunito', fontSize : 14, color : '#061946', fontWeight : "800"}, 
-          div : {fontFamily : 'Nunito', fontSize : 18, color : '#061946', fontWeight : "800"}, 
-          img : {maxWidth : width - 50, height : 'auto'},
-          td : {borderWidth : 1, borderColor : '#061946', padding : 5},
-          table : {width : '100%'}
-        }
-      }
-      renderersProps={renderersProps}
-    />
+    <ScrollView 
+      style={[props.style]} 
+      pointerEvents="none" 
+      removeClippedSubviews={true}
+      overScrollMode='never'
+    >
+      {webViewHeight === 0 && <ActivityIndicator />}
+      <WebView
+        style={[styles.webview, {height: webViewHeight, opacity : 0.99}]}
+        containerStyle={[{height: webViewHeight}]}
+        onMessage={onWebViewMessage}
+        textZoom={100}
+        source={{
+          html: `
+            <html>
+              <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0">
+                <style>
+                  ${fontFace('Nunito-Light', 'ttf', 'Nunito', 'lighter')}
+                  ${fontFace('Nunito-Regular', 'ttf', 'Nunito', 'regular')}
+                  ${fontFace('Nunito-Bold', 'ttf', 'Nunito', 'bold')}
+                  ${fontFace('Nunito-ExtraBold', 'ttf', 'Nunito', 'bolder')}
+
+                  ${normalize}
+
+                  *,
+                  *::before,
+                  *::after {
+                    box-sizing: border-box;
+                    max-width: 100vw;
+                  }
+                  *:empty,
+                  *:blank {
+                    display: none;
+                  }
+                  img {
+                    display: block;
+                    max-width:95%;
+                    width: auto;
+                    height: auto;
+                  }
+                  table {
+                    margin: 1rem 0;
+                    width : 100% !important;
+                  }
+                  td, tr, th {
+                    padding: 10px;
+                    max-width: 40%;
+                  }
+                  #content {
+                    font-size: 18;
+                    color: #061946;
+                    font-family: 'Nunito', sans-serif;
+                    padding-right: .125rem;
+                  }
+                  ${props.css}
+                </style>
+              </head>
+              <body>
+                <main id="content">
+                  ${HtmlCleaner(props.html)}
+                </main>
+              </body>
+            </html>
+          `,
+        }}
+        scrollEnabled={false}
+        injectedJavaScript={`
+          (function() {
+            setTimeout(function() {
+              const height = document.getElementById('content').scrollHeight;
+              window.ReactNativeWebView.postMessage(height)
+            }, 300)
+          })();
+        `}
+      />
+
+    </ScrollView>
   );
 };
 
